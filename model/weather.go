039df0e67539c7f7e5a2e55581bf41c8d7a3ea5b
coding-to-music <connors.tom@gmail.com>
2023-09-17 00:28:44 -0500

    Build

--- model/weather.go
+++ model/weather.go
@@ -11,11 +11,20 @@ type Weather struct {
 	TimezoneOffsetSeconds int64      `json:"timezone_offset_seconds"`
 	Condition             string     `json:"condition"`
 	Icon                  string     `json:"icon"`
+
 	AvgTempC              float64    `json:"temp_c"`
+	AvgTempF              float64    `json:"temp_f"`
 	MinTempC              float64    `json:"min_temp_c"`
+	MinTempF              float64    `json:"min_temp_f"`
 	MaxTempC              float64    `json:"max_temp_c"`
+	MaxTempF              float64    `json:"max_temp_f"`
+
 	AvgWindKph            float64    `json:"avg_wind_kph"`
+	AvgWindMph            float64    `json:"avg_wind_mph"`
 	MinWindKph            float64    `json:"min_wind_kph"`
+	MinWindMph            float64    `json:"min_wind_mph"`
 	MaxWindKph            float64    `json:"max_wind_kph"`
+	MaxWindMph            float64    `json:"max_wind_mph"`
+
 	HourlyWeathers        []Weather  `json:"hourly_weathers"`
 }
